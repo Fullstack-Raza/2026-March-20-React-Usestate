74be50ebf0aca0d883af5b098bddeebe56eeebb6
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-03-20 13:53:49 +0500

    Hello

---
+++ use-state/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-red-800">
+      <h1 className="text-4xl">Hello Guys</h1>
+    </div>
+  )
+}
+
+export default App
